d4449817af1bbf42349d8e0a9297b577d452177e
Nikita Menshutin <info@nikita.global>
2020-12-19 23:14:43 +0200

    theme updated

--- toTheme/functions.php
+++ toTheme/functions.php
@@ -3,6 +3,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 define('THEMEPREFIX', 'THEMEPREFIXHERE');
 define('THEMEVERSION', '1.0');
+define('THEMESCRIPTS', []);
+define('THEMESTYLES', []);
 
 class NgTheme extends Wptheme\Theme {
     function __construct() {
