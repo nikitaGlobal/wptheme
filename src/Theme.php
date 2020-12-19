558547019be5891ce465bca148c8e753aac9a75e
Nikita Menshutin <info@nikita.global>
2020-12-19 22:26:24 +0200

    theme updated

--- src/Theme.php
+++ src/Theme.php
@@ -1,9 +1,43 @@
 <?php
 namespace Wptheme;
 
-class Theme {
+class Theme
+{
     public function __construct()
     {
-        echo 123;
+            add_theme_support("editor-style");
+            add_theme_support("post-thumbnails");
+            $this->init();
+    }
+
+    public function init()
+    {
+
+            $methods = get_class_methods($this);
+        if (empty($methods)) {
+            return;
+        }
+        foreach ($methods as $method) {
+            if (strpos($method, 'init') === 0) {
+                $this->{$method}();
+            }
+            if (strpos($method, 'filter') === 0) {
+                $filter = str_replace('filter', '', $method);
+                add_filter($filter, array($this, $method));
+            }
+            if (strpos($method, 'sc') === 0) {
+                $shortcode = str_replace('sc', self::prefix(), $method);
+                add_shortcode($shortcode, array($this, $method));
+            }
+        }
+    }
+
+    public static function version()
+    {
+        if (defined('WP_DEBUG') && WP_DEBUG == true) {
+            return time();
+        }
+            
+        return THEMEVERSION;
     }
 }
--- toTheme/functions.php
+++ toTheme/functions.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-define('themeprefix', 'THEMEPREFIXHERE');
+define('THEMEPREFIX', 'THEMEPREFIXHERE');
+define('THEMEVERSION', '1.0');
 
 class NgTheme extends Wptheme\Theme {
     function __construct() {
