1b035ec42817fc969f09d88214d506d4403e80c1
Nikita Menshutin <nikita.menshutin@ns-mediagroup.com>
2022-04-21 11:48:02 +0300

    chore: flat carbonfields

--- src/Theme.php
+++ src/Theme.php
@@ -58,7 +58,7 @@ class Theme {
 				self::version(),
 				true
 			);
-				wp_enqueue_script( self::sanitize( $scr ) );
+			wp_enqueue_script( self::sanitize( $scr ) );
 		}
 	}
 
@@ -75,7 +75,7 @@ class Theme {
 				self::version(),
 				'all'
 			);
-				wp_enqueue_style( self::sanitize( $scr ) );
+			wp_enqueue_style( self::sanitize( $scr ) );
 		}
 	}
 
@@ -89,24 +89,32 @@ class Theme {
 	}
 
 	public function action_carbon_fields_register_fields() {
-		// Поля для custom post types
-		foreach ( TYPE_FIELDS as $type_name => $type_arr ) {
-			$fields = array();
-			foreach ( $type_arr as $arr_field ) {
-				// [0] - тип поля, [1] - название поля, [2] - метка поля
-				$fields[] = Field::make( $arr_field[0], $arr_field[1], $arr_field[2] );
-			}
-			Container::make( 'post_meta', __( 'Дополнительно' ) )
+		if ( defined( 'TYPE_FIELDS' ) ) {
+			foreach ( TYPE_FIELDS as $type_name => $type_arr ) {
+				$fields = array();
+				foreach ( $type_arr as $arr_field ) {
+					$fields[] = $this->carbon_field_make( $arr_field );
+				}
+				Container::make( 'post_meta', __( 'Custom fields' ) )
 				->where( 'post_type', '=', $type_name )
 				->add_fields( $fields );
+			}
 		}
-		// Поля настроек темы
-		$fields = array();
-		foreach ( SETTINGS_FIELDS as $sets_item ) {
-			$fields[] = Field::make( $sets_item[0], $sets_item[1], $sets_item[2] );
-		}
-		Container::make( 'theme_options', __( 'Настройки темы' ) )
+		if ( defined( 'SETTINGS_FIELDS' ) ) {
+			$fields = array();
+			foreach ( SETTINGS_FIELDS as $sets_item ) {
+				$fields[] = $this->carbon_field_make( $sets_item );
+			}
+			Container::make( 'theme_options', __( 'Theme Options' ) )
 			->add_fields( $fields );
+		}
+	}
+
+	private function carbon_field_make( $arr_field ) {
+		if ( isset( $arr_field[3] ) && $arr_field[3] ) {
+			return Field::make( $arr_field[0], $arr_field[1], $arr_field[2] )->set_options( $arr_field[3] );
+		}
+		return Field::make( $arr_field[0], $arr_field[1], $arr_field[2] );
 	}
 
 	public static function tags( $pid = false ) {
@@ -129,18 +137,6 @@ class Theme {
 		$names     = preg_split( '/(?=[A-Z])/', $name );
 		$className = 'Wptheme\\' . ucfirst( $names[0] );
 		return $className::{$names[1]}( ...$arguments );
-	}
-
-	public function action_after_setup_theme() {
-		 \Carbon_Fields\Carbon_Fields::boot();
-	}
-
-	public static function scriptPath( $file ) {
-		if ( 0 !== strpos( $file, 'http' ) ) {
-			return get_bloginfo( 'template_url' ) . '/' . preg_replace( '/^\//', '', $file );
-		}
-
-		return $file;
 	}
 
 	public static function Share( $network, $link = false ) {
@@ -171,6 +167,17 @@ class Theme {
 	public static function getCurrentLink() {
 		global $wp;
 		return add_query_arg( $_GET, home_url( $wp->request ) );
+	}
+	public function action_after_setup_theme() {
+		\Carbon_Fields\Carbon_Fields::boot();
+	}
+
+	public static function scriptPath( $file ) {
+		if ( 0 !== strpos( $file, 'http' ) ) {
+			return get_bloginfo( 'template_url' ) . '/' . preg_replace( '/^\//', '', $file );
+		}
+
+		return $file;
 	}
 
 	public static function sanitize( $value ) {
