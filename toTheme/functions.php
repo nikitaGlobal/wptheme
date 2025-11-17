540f3ab0b41aed03559b7c4841abae2b606bce1b
Nikita Menshutin <nikita.menshutin@edited.com>
2025-11-17 12:55:46 +0200

    chore: Tags fixed, code linted

--- toTheme/functions.php
+++ toTheme/functions.php
@@ -1,10 +1,25 @@
 <?php
+/**
+ * Theme functions and definitions.
+ *
+ * @package NgTheme
+ *
+ * @codingStandardsIgnoreFile WordPress.Files.FileName.InvalidClassFileName
+ */
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 require __DIR__ . '/theme.php';
 
+/**
+ * Main theme class.
+ */
 class NgTheme extends Wptheme\Theme {
-	function __construct() {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
 		parent::__construct();
 	}
 }
